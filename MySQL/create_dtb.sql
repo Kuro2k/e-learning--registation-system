fffd246701fd7b57ea02259899aaba05cf2a334f
Kuro2k <dinhgiaquang2000@gmail.com>
2021-11-09 22:22:47 +0700

    update

--- MySQL/create_dtb.sql
+++ MySQL/create_dtb.sql
@@ -24,24 +24,90 @@ CREATE TABLE supporter(
     salary INT,
     working_time TIME,
     CHECK (salary > 0),
-    CHECK (working_time >= 8 and working_time <= 18),
+    CHECK (working_time >= '8:00:00' and working_time <= '18:00:00'),
     PRIMARY KEY (ID),
-    FOREIGN KEY (ID) REFERENCES user_system(id)
+    FOREIGN KEY (ID) REFERENCES user_system(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE learner(
 	id	CHAR(7),
     join_date	DATE NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES user_system(id)
+    FOREIGN KEY (id) REFERENCES user_system(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE lecturer(
 	id 	CHAR(7),
     salary 	INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES user_system(id) 
+    FOREIGN KEY (id) REFERENCES user_system(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+-- Check disjoiness in learner and lecture
+DELIMITER \\
+CREATE TRIGGER check_disjoin_supporter
+BEFORE INSERT ON supporter
+FOR EACH ROW
+IF (NEW.id IN (SELECT id FROM learner) OR NEW.id IN (SELECT id FROM lecturer))
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'This new id have been in lecture or leaner';
+END IF \\
+
+DELIMITER ;
+--
+DELIMITER \\
+CREATE TRIGGER check_disjoin_supporter_2
+BEFORE UPDATE ON supporter
+FOR EACH ROW
+IF (NEW.id IN (SELECT id FROM learner) OR NEW.id IN (SELECT id FROM lecturer))
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'This new id have been in lecture or leaner';
+END IF \\
+
+DELIMITER ;
+-- Check disjoin in supporter and lecturer
+DELIMITER \\
+CREATE TRIGGER check_disjoin_learner
+BEFORE INSERT ON learner
+FOR EACH ROW
+IF (NEW.id IN (SELECT id FROM supporter) OR NEW.id IN (SELECT id FROM lecturer))
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'This new id have been in lecture or supporter';
+END IF \\
+
+DELIMITER ;
+--
+DELIMITER \\
+CREATE TRIGGER check_disjoin_learner_2
+BEFORE UPDATE ON learner
+FOR EACH ROW
+IF (NEW.id IN (SELECT id FROM supporter) OR NEW.id IN (SELECT id FROM lecturer))
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'This new id have been in lecture or supporter';
+END IF \\
+
+DELIMITER ;
+-- Check disjoin in supporter and learner
+DELIMITER \\
+CREATE TRIGGER check_disjoin_lecturer
+BEFORE INSERT ON lecturer
+FOR EACH ROW
+IF (NEW.id IN (SELECT id FROM supporter) OR NEW.id IN (SELECT id FROM learner))
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'This new id have been in supporter or leaner';
+END IF \\
+
+DELIMITER ;
+--
+DELIMITER \\
+CREATE TRIGGER check_disjoin_lecturer_2
+BEFORE UPDATE ON lecturer
+FOR EACH ROW
+IF (NEW.id IN (SELECT id FROM supporter) OR NEW.id IN (SELECT id FROM learner))
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'This new id have been in supporter or leaner';
+END IF \\
+
+DELIMITER ;
 
 CREATE TABLE field(
 	id CHAR(4),
@@ -51,7 +117,7 @@ CREATE TABLE field(
 
 CREATE TABLE course(
 	id CHAR(6),
-    name VARCHAR(64),
+    name VARCHAR(128),
     tuition_fee DECIMAL(10,2),
     start_date DATE,
     course_length INT,
@@ -66,73 +132,99 @@ CREATE TABLE documents(
     name 	CHAR(64) NOT NULL,
     type 	VARCHAR(24), 
     PRIMARY KEY (courseid, serial),
-    FOREIGN KEY (courseid) REFERENCES course(id)
+    FOREIGN KEY (courseid) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE test(
 	courseid CHAR(6),
-    testid	CHAR(3),
-    type 	VARCHAR(24),
+    testid	VARCHAR(4),
+    type 	VARCHAR(64),
     duration INT NOT NULL,
     CHECK (duration >= 15 and duration <= 200),
     PRIMARY KEY (courseid, testid),
-    FOREIGN KEY (courseid) REFERENCES course(id)
+    FOREIGN KEY (courseid) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 ALTER TABLE learner
 ADD COLUMN (
 	supportid CHAR(7) NOT NULL,
-	FOREIGN KEY (supportid) REFERENCES user_system(id)
+	FOREIGN KEY (supportid) REFERENCES user_system(id) ON UPDATE CASCADE ON DELETE CASCADE
 	);
     
 CREATE TABLE enrolls(
 	learnerid	CHAR(7),
     courseid	CHAR(6),
     PRIMARY KEY (learnerid, courseid),
-    FOREIGN KEY (learnerid) REFERENCES learner(id),
-    FOREIGN KEY (courseid)	REFERENCES course(id)
+    FOREIGN KEY (learnerid) REFERENCES learner(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (courseid)	REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE rates(
 	learnerid	CHAR(7),
     courseid	CHAR(6),
     PRIMARY KEY (learnerid, courseid),
-    FOREIGN KEY (learnerid) REFERENCES learner(id),
-    FOREIGN KEY (courseid)	REFERENCES course(id)
+    FOREIGN KEY (learnerid) REFERENCES learner(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (courseid)	REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE teaches(
 	lecturerid	CHAR(7),
     courseid	CHAR(6),
     PRIMARY KEY (lecturerid, courseid),
-    FOREIGN KEY (lecturerid) REFERENCES lecturer(id),
-    FOREIGN KEY (courseid) REFERENCES course(id)
+    FOREIGN KEY (lecturerid) REFERENCES lecturer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (courseid) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+-- check total participation in Course
+DELIMITER \\
+CREATE TRIGGER check_total_course
+BEFORE DELETE ON teaches
+FOR EACH ROW
+IF ((SELECT COUNT(*)
+	 FROM teaches
+     WHERE teaches.courseid = OLD.courseid) <= 1)
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'Remain the only. Please delete from course table';
+END IF \\
+
+DELIMITER ;
 
 CREATE TABLE course_in_field(
 	fieldid 	CHAR(4),
     courseid	CHAR(6),
     PRIMARY KEY (fieldid, courseid),
-    FOREIGN KEY (fieldid) REFERENCES field(id),
-    FOREIGN KEY (courseid) REFERENCES course(id)
+    FOREIGN KEY (fieldid) REFERENCES field(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (courseid) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+-- check total course_in_field
+DELIMITER \\
+CREATE TRIGGER check_total_course_2
+BEFORE DELETE ON course_in_field
+FOR EACH ROW
+IF ((SELECT COUNT(*)
+	 FROM course_in_field
+     WHERE course_in_field.courseid = OLD.courseid) <= 1)
+THEN SIGNAL SQLSTATE '45000'
+	 SET MESSAGE_TEXT = 'Remain the only. Please delete from course table';
+END IF \\
+
+DELIMITER ;
 
 CREATE TABLE prerequires(
 	precourseid 	CHAR(6),
     courseid		CHAR(6),
     PRIMARY KEY (precourseid, courseid),
-    FOREIGN KEY (precourseid) REFERENCES course(id),
-    FOREIGN KEY (courseid) REFERENCES course(id)
+    FOREIGN KEY (precourseid) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (courseid) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE takes(
 	learnerid 	CHAR(7),
     courseid	CHAR(6),
-    testid		CHAR(3), 
+    testid		VARCHAR(4), 
     test_date	DATE NOT NULL,
     score		INT NOT NULL,
     PRIMARY KEY (learnerid, courseid, testid),
-    FOREIGN KEY (courseid, testid) REFERENCES test(courseid, testid),
+    FOREIGN KEY (courseid, testid) REFERENCES test(courseid, testid) ON UPDATE CASCADE ON DELETE CASCADE,
     CHECK (score >= 0 and score <= 10)
 );
 
@@ -145,7 +237,7 @@ CREATE TABLE enrolls_info(
     fee 	DECIMAL(10,2) NOT NULL,
     start_date DATE,
     PRIMARY KEY (learnerid, courseid, enrollment_date),
-    FOREIGN KEY (learnerid, courseid) REFERENCES enrolls(learnerid, courseid),
+    FOREIGN KEY (learnerid, courseid) REFERENCES enrolls(learnerid, courseid) ON UPDATE CASCADE ON DELETE CASCADE,
     CHECK (payment_status in ("Complete", "Pending", "Canceled")),
     CHECK (enrollment_status in ("Enrolled", "Postponed", "Canceled")),
     CHECK (fee > 0)
@@ -158,7 +250,7 @@ CREATE TABLE details_rates(
     star_num	INT NOT NULL,
     comment 	VARCHAR(1024),
     PRIMARY KEY (learnerid, courseid, evaluation_date),
-    FOREIGN KEY (learnerid, courseid) REFERENCES rates(learnerid, courseid),
+    FOREIGN KEY (learnerid, courseid) REFERENCES rates(learnerid, courseid) ON UPDATE CASCADE ON DELETE CASCADE,
     CHECK (star_num >= 1 and star_num <= 5)
 );
 
@@ -167,7 +259,7 @@ CREATE TABLE degree(
     field		VARCHAR(128),
     level		VARCHAR(128),
     PRIMARY KEY (lecturerid, field, level),
-    FOREIGN KEY (lecturerid) REFERENCES lecturer(id)
+    FOREIGN KEY (lecturerid) REFERENCES lecturer(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -177,5 +269,5 @@ CREATE TABLE support_session(
     finish_time DATETIME,
     content		VARCHAR(1024),
     PRIMARY KEY (learnerid, start_time, finish_time),
-    FOREIGN KEY (learnerid) REFERENCES learner(id)
+    FOREIGN KEY (learnerid) REFERENCES learner(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
